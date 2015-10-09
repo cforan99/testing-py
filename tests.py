7b7626c0bdfcb57ec58c089d4fbe656d3a46cfc3
Hackbright Student <info@hackbrightacademy.com>
2015-10-09 13:01:07 -0700

    Completed most further study unittests

---
+++ tests.py
@@ -0,0 +1,36 @@
+import unittest
+
+import party
+
+
+class PartyTests(unittest.TestCase):
+    """Tests for my party site."""
+
+    def setUp(self):
+        self.client = party.app.test_client()
+
+    def test_homepage(self):
+        result = self.client.get("/")
+        self.assertIn("I'm having a party", result.data)
+
+    def test_no_rsvp_yet(self):
+        result = self.client.get("/")
+        self.assertIn("Please RSVP", result.data)
+        self.assertNotIn("Party Details", result.data)
+
+    def test_rsvp(self):
+        result = self.client.post("/rsvp",
+                                  data={'name': "Jane", 'email': "jane@jane.com"},
+                                  follow_redirects=True)
+        self.assertNotIn("Please RSVP", result.data)
+        self.assertIn("Party Details", result.data)
+
+    def test_rsvp_mel(self):
+        result = self.client.post("/rsvp",
+                                  data={'name': "Mel Melitpolski", 'email': "mel@ubermelon.com"},
+                                  follow_redirects=True)
+        self.assertIn("awkward", result.data)
+
+
+if __name__ == "__main__":
+    unittest.main()
